b4f921f9e77b1ab958eb5692e8f8b92feb3af339
prakhar pandey <prakhar9704@gmail.com>
2025-09-23 19:41:15 +0530

    improved secure of otp

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -10,8 +10,8 @@ const mailSender=async function(email,title,body){
     console.log("inside try of mail sender");
         let transporter=nodemailer.createTransport({
             host:process.env.MAIL_HOST, 
-            port:465,
-            secure:true,
+            port:587,
+            secure:false,
             auth:{
                 user:process.env.MAIL_USER,
                 pass:process.env.MAIL_PASS,
